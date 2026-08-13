44ac35c63a3a2a3e5df3b6ca56883ff37a09485b
vaishnavishelke9604-sudo <vaishnavishelke9604@gmail.com>
2026-08-13 19:11:51 +0530

    fronend change

--- src/main/java/com/medicalReportOcr/security/SecurityConfig.java
+++ src/main/java/com/medicalReportOcr/security/SecurityConfig.java
@@ -6,16 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
 	
-       @Bean
+	
+
+
+
+	    @Bean
 	    public SecurityFilterChain securityFilterChain(
 	            HttpSecurity http) throws Exception {
 
 	        http
 	            .csrf(csrf -> csrf.disable())
+
+	            .cors(cors -> {})
 
 	            .sessionManagement(session ->
 	                session.sessionCreationPolicy(
@@ -24,6 +35,12 @@ public class SecurityConfig {
 	            )
 
 	            .authorizeHttpRequests(auth -> auth
+
+	                .requestMatchers(
+	                    HttpMethod.OPTIONS,
+	                    "/**"
+	                ).permitAll()
+
 	                .requestMatchers(
 	                    "/extract",
 	                    "/actuator/health",
@@ -31,12 +48,49 @@ public class SecurityConfig {
 	                    "/v3/api-docs/**"
 	                ).permitAll()
 
-	                .anyRequest().authenticated()
+	                .anyRequest()
+	                .authenticated()
 	            );
 
 	        return http.build();
 	    }
+
+	    @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+
+	        CorsConfiguration configuration =
+	                new CorsConfiguration();
+
+	        configuration.setAllowedOrigins(List.of(
+	            "https://medicalocr-frondend.onrender.com",
+	            "http://localhost:5500",
+	            "http://127.0.0.1:5500"
+	        ));
+
+	        configuration.setAllowedMethods(List.of(
+	            "GET",
+	            "POST",
+	            "PUT",
+	            "DELETE",
+	            "OPTIONS"
+	        ));
+
+	        configuration.setAllowedHeaders(List.of("*"));
+
+	        configuration.setAllowCredentials(false);
+
+	        UrlBasedCorsConfigurationSource source =
+	                new UrlBasedCorsConfigurationSource();
+
+	        source.registerCorsConfiguration(
+	            "/**",
+	            configuration
+	        );
+
+	        return source;
+	    }
 	}
+	
 	
 	
 	
